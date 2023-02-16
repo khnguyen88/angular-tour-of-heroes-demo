9a332b279d110e876e1284d400590765f0998371
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-02-16 01:38:31 -0500

    Imported the MessageService into the HeroesComponent. Added a constructor for the HeroesComponent class with a parameter that declares a private messageService property.
    Updated the onSelect method to adds a new message to messageService's messages everytime user selects a hero.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -28,7 +28,6 @@ export class HeroesComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-
   }
 
   getHeroes(): void {
