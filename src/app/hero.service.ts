7a55d08f097fdc06d0b54c54aaf90a3ca12bd39a
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-02-17 14:28:28 -0500

    Imported HttpClient and HttpHeaders

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
