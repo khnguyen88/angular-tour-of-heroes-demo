2e83d528de3392bc3382eae6a3e9a617ca384bf9
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-02-17 17:46:33 -0500

    Updated the getHero() method to  to construct a request URL with the desired hero's id.
    Most web AP ls support a get by id request in the form : baseURL/ : id . Here, the base URL is the heroesURL defined in the Heroes and HTTP section in api/heroes and id is the number of the hero that you want to retrieve.
    
    The server should respond with a single hero rather than an array of heroes. getHero( ) returns an Obse rvable<Hero>, which is an observable of Hero objects rather than an observable of Hero arrays.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -31,11 +31,11 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified "id' always exists .
-    // Error handling will be added in the next step of the tutorial .
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroServices: fetched hero ${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /** Log a HeroService message with the MessageService */
